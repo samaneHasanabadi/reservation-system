39ecbb373ef082268be8a1de9dd90e6333b0ee52
Samane <Smn@gmail.com>
2025-12-03 12:36:35 +0330

    init: initialize Spring Boot Reservation System project

---
+++ src/test/java/ir/azki/reservationsystem/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.azki.reservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
