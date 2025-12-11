3bba4f3b763970a21b4d571176751b34aaa1574f
Samane <Smn@gmail.com>
2025-12-11 17:03:01 +0330

    fix(api) : remove extra field in register request

--- src/main/java/ir/azki/reservationsystem/user/api/resources/RegisterUserRequest.java
+++ src/main/java/ir/azki/reservationsystem/user/api/resources/RegisterUserRequest.java
@@ -5,9 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record RegisterUserRequest(
-        @NotBlank(message = "Name is required")
-        String name,
-
         @NotBlank(message = "Username is required")
         @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
         String username,
