a3d2d8c6f8e66cf7e6fefbac7d10f65773002187
Samane <Smn@gmail.com>
2025-12-11 17:26:41 +0330

    feat(slot) : add update slot service

--- src/main/java/ir/azki/reservationsystem/slot/api/SlotCommandController.java
+++ src/main/java/ir/azki/reservationsystem/slot/api/SlotCommandController.java
@@ -4,6 +4,7 @@ import ir.azki.reservationsystem.slot.api.resources.CreateSlotRequest;
 import ir.azki.reservationsystem.slot.application.CreateSlotCommand;
 import ir.azki.reservationsystem.slot.application.handler.CreateSlotCommandHandler;
 import ir.azki.reservationsystem.slot.application.handler.DeleteSlotCommandHandler;
+import ir.azki.reservationsystem.slot.application.handler.UpdateSlotCommandHandler;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.convert.ConversionService;
@@ -11,12 +12,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
+
 @RestController
 @RequestMapping("/api/slot")
 @RequiredArgsConstructor
 public class SlotCommandController {
 
     private final CreateSlotCommandHandler createSlotCommandHandler;
+    private final UpdateSlotCommandHandler updateSlotCommandHandler;
     private final DeleteSlotCommandHandler deleteSlotCommandHandler;
     private final ConversionService conversionService;
 
@@ -25,6 +29,13 @@ public class SlotCommandController {
         CreateSlotCommand convert = conversionService.convert(request, CreateSlotCommand.class);
         createSlotCommandHandler.handle(convert);
         return ResponseEntity.status(HttpStatus.CREATED).body("Slot is successfully created");
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<String> updateBudget(@PathVariable Long id, @Valid @RequestBody CreateSlotRequest request) throws AccessDeniedException {
+        CreateSlotCommand command = conversionService.convert(request, CreateSlotCommand.class);
+        updateSlotCommandHandler.handle(id, command);
+        return ResponseEntity.ok().body("Slot is updated successfully!");
     }
 
     @DeleteMapping("/delete/{id}")
---
+++ src/main/java/ir/azki/reservationsystem/slot/application/handler/UpdateSlotCommandHandler.java
@@ -0,0 +1,37 @@
+package ir.azki.reservationsystem.slot.application.handler;
+
+import ir.azki.reservationsystem.slot.application.CreateSlotCommand;
+import ir.azki.reservationsystem.slot.domain.Slot;
+import ir.azki.reservationsystem.slot.domain.SlotRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UpdateSlotCommandHandler {
+
+    private final SlotRepository slotRepository;
+
+    @Transactional
+    public void handle(Long id, CreateSlotCommand command) {
+        Slot slot = slotRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("slot not found with id : " + id));
+
+        if (slot.getIsReserved())
+            throw new IllegalArgumentException("Slot is reserved and cannot be updated");
+
+        if (!command.start().before(command.end()))
+            throw new IllegalArgumentException("Start date must be before end date");
+
+        List<Slot> overlappedSlots = slotRepository.findOverlappedSlots(command.start(), command.end());
+        if (overlappedSlots.stream().anyMatch(s -> !s.getId().equals(id)))
+            throw new IllegalArgumentException("time slot is overlapped with available time slots");
+
+        slot.setStart(command.start());
+        slot.setEnd(command.end());
+        slotRepository.save(slot);
+    }
+
+}
